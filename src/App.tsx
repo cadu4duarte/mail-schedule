71098566a0bfbe1a888822608a4728aa7516246e
Carlos Eduardo <cadu4duarte@gmail.com>
2023-01-06 15:31:55 -0300

    feat: import nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Nav } from "./components";
 
 export default function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Nav />
+    </div>
   )
 }
 
