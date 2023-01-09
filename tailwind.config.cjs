c8e269fdcd72b59e7e2ef9b368e7ee135f834904
Carlos Eduardo <cadu4duarte@gmail.com>
2023-01-09 14:24:45 -0300

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryLight: "hsla(212,100%,75%,1)",
+        primary: "hsla(212,100%,65%,1)",
+        primaryDark: "hsla(212,100%,35%,1)"
+      },
+    },
   },
   plugins: [],
 }
