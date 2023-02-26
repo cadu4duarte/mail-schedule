c92f93c244e19884b18804db65a1a2539e59c51a
Carlos Eduardo <cadu4duarte@gmail.com>
2023-02-26 20:59:38 -0300

    feat: add new button

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Button } from "../Button/Button";
 import { NavLogo } from "./NavLogo";
 
@@ -6,10 +7,17 @@ export function Nav() {
     <nav className="w-full h-20 bg-gray-50 border-b border-gray-200">
       <div className="w-full h-full max-w-7xl m-auto flex items-center justify-between">
         <NavLogo />
+        <div className="flex items-center gap-4">
+          <Link to="/dashboard">
+            <Button>Dashboard</Button>
+          </Link>
+          <Link to="/escrever-agora">
+            <Button variant="primary">
+              Escrever agora
+            </Button>
+          </Link>
 
-        <Button variant="primary">
-          Escrever agora
-        </Button>
+        </div>
       </div>
     </nav>
   )
