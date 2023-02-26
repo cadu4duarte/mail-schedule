191dd28ae482b24451335cdb7feda41a5de682cb
Carlos Eduardo <cadu4duarte@gmail.com>
2023-02-26 20:58:37 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Nav } from "./components/Nav/Nav";
+import { AppRoutes } from "./routes";
 
 export default function App() {
 
   return (
-    <div>
-      <Nav />
-    </div>
+    <AppRoutes />
   )
 }
 
