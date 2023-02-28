a60c51379a220160684b0a82767ed06b06ee7155
Carlos Eduardo <cadu4duarte@gmail.com>
2023-02-28 09:51:36 -0300

    feat: edit layout form

--- src/pages/WriteNow.tsx
+++ src/pages/WriteNow.tsx
@@ -1,33 +1,45 @@
+import { FormProvider, useForm } from "react-hook-form";
 import { Button } from "../components/Button/Button";
+import { DateTimerPicker } from "../components/Form/DateTimePicker";
+import { RichInput } from "../components/Form/Richinput";
 import { Nav } from "../components/Nav/Nav";
 
 export function WriteNowPage() {
-  return(
+  const formMethods = useForm();
+  const { register, handleSubmit } = formMethods;
+
+  function onSubmit(values: any) {
+    console.log(values)
+  }
+
+  return (
     <>
       <Nav />
       <div className="container">
         <h1>Escrever agora</h1>
 
-        <form className="form">
-          <label htmlFor="destinationName">Nome Completo</label>
-          <input type="text" />
+        <FormProvider {...formMethods}>
+          <form className="form" onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="destinationName">Nome Completo</label>
+            <input {...register("destinationName")} type="text" />
 
-          <label htmlFor="destinationAddress">E-mail</label>
-          <input type="email" />
+            <label htmlFor="destinationAddress">E-mail</label>
+            <input {...register("destinationAddress")} type="email" />
 
-          <label htmlFor="dueDate">Data</label>
-          <input type="text" />
+            <label htmlFor="dueDate">Data</label>
+            <DateTimerPicker name="dueDate" />
 
-          <label htmlFor="subject">Assunto</label>
-          <input type="text" />
+            <label htmlFor="subject">Assunto</label>
+            <input {...register("subject")} type="text" />
 
-          <label htmlFor="body">Mensagem</label>
-          <input type="text" />
+            <label htmlFor="body">Mensagem</label>
+            <RichInput name="body" />
 
-          <Button type="submit" variant="primary">Enviar</Button>
-        </form>
+            <Button type="submit" variant="primary">Enviar</Button>
+          </form>
+        </FormProvider>
       </div>
-      
+
     </>
   )
 }
