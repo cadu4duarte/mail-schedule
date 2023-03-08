4c9a92473927582bef56d0a10b0ab384ceb6dfef
Carlos Eduardo <cadu4duarte@gmail.com>
2023-03-08 17:51:07 -0300

    feat: import a new components

--- src/pages/WriteNow.tsx
+++ src/pages/WriteNow.tsx
@@ -3,10 +3,12 @@ import { Button } from "../components/Button/Button";
 import { DateTimerPicker } from "../components/Form/DateTimePicker";
 import { RichInput } from "../components/Form/Richinput";
 import { Nav } from "../components/Nav/Nav";
+import { IWriteNowForm } from "../interfaces/IWriteNowForm";
+import { WriteNowResolver } from "../validations/WriteNotValidation";
 
 export function WriteNowPage() {
-	const formMethods = useForm();
-	const { register, handleSubmit } = formMethods;
+	const formMethods = useForm<IWriteNowForm>({ resolver: WriteNowResolver });
+	const { formState: { errors }, register, handleSubmit } = formMethods;
 
 	function onSubmit(values: any) {
 		console.log(values)
@@ -22,20 +24,37 @@ export function WriteNowPage() {
 					<form className="form" onSubmit={handleSubmit(onSubmit)}>
 						<label htmlFor="destinationName">Nome Completo</label>
 						<input {...register("destinationName")} type="text" />
+						{errors?.destinationName?.message && (
+							<p className="error-text">{errors?.destinationName?.message}</p>
+						)}
 
 						<label htmlFor="destinationAddress">E-mail</label>
 						<input {...register("destinationAddress")} type="email" />
+						{errors?.destinationAddress?.message && (
+							<p className="error-text">{errors?.destinationAddress?.message}</p>
+						)}
 
 						<label htmlFor="dueDate">Data</label>
 						<DateTimerPicker name="dueDate" />
+						{errors?.dueDate?.message && (
+							<p className="error-text">{errors?.dueDate?.message}</p>
+						)}
 
 						<label htmlFor="subject">Assunto</label>
 						<input {...register("subject")} type="text" />
+						{errors?.subject?.message && (
+							<p className="error-text">{errors?.subject?.message}</p>
+						)}
 
 						<label htmlFor="body">Mensagem</label>
 						<RichInput name="body" />
+						{errors?.body?.message && (
+							<p className="error-text">{errors?.body?.message}</p>
+						)}
 
-						<Button type="submit" variant="primary">Enviar</Button>
+						<Button type="submit" variant="primary">
+							Enviar
+						</Button>
 					</form>
 				</FormProvider>
 			</div>
