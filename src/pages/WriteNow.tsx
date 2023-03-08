1022b1dd52408bda28c3a7e1e9b78f66bb32dfcb
Carlos Eduardo <cadu4duarte@gmail.com>
2023-03-08 10:46:51 -0300

    feat: edit code layout

--- src/pages/WriteNow.tsx
+++ src/pages/WriteNow.tsx
@@ -5,41 +5,41 @@ import { RichInput } from "../components/Form/Richinput";
 import { Nav } from "../components/Nav/Nav";
 
 export function WriteNowPage() {
-  const formMethods = useForm();
-  const { register, handleSubmit } = formMethods;
+	const formMethods = useForm();
+	const { register, handleSubmit } = formMethods;
 
-  function onSubmit(values: any) {
-    console.log(values)
-  }
+	function onSubmit(values: any) {
+		console.log(values)
+	}
 
-  return (
-    <>
-      <Nav />
-      <div className="container">
-        <h1>Escrever agora</h1>
+	return (
+		<>
+			<Nav />
+			<div className="container">
+				<h1>Escrever agora</h1>
 
-        <FormProvider {...formMethods}>
-          <form className="form" onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="destinationName">Nome Completo</label>
-            <input {...register("destinationName")} type="text" />
+				<FormProvider {...formMethods}>
+					<form className="form" onSubmit={handleSubmit(onSubmit)}>
+						<label htmlFor="destinationName">Nome Completo</label>
+						<input {...register("destinationName")} type="text" />
 
-            <label htmlFor="destinationAddress">E-mail</label>
-            <input {...register("destinationAddress")} type="email" />
+						<label htmlFor="destinationAddress">E-mail</label>
+						<input {...register("destinationAddress")} type="email" />
 
-            <label htmlFor="dueDate">Data</label>
-            <DateTimerPicker name="dueDate" />
+						<label htmlFor="dueDate">Data</label>
+						<DateTimerPicker name="dueDate" />
 
-            <label htmlFor="subject">Assunto</label>
-            <input {...register("subject")} type="text" />
+						<label htmlFor="subject">Assunto</label>
+						<input {...register("subject")} type="text" />
 
-            <label htmlFor="body">Mensagem</label>
-            <RichInput name="body" />
+						<label htmlFor="body">Mensagem</label>
+						<RichInput name="body" />
 
-            <Button type="submit" variant="primary">Enviar</Button>
-          </form>
-        </FormProvider>
-      </div>
+						<Button type="submit" variant="primary">Enviar</Button>
+					</form>
+				</FormProvider>
+			</div>
 
-    </>
-  )
+		</>
+	)
 }
